e8d7f064a2cac0bbff3998070493e2e963ca9446
gaseet <45747276+gaseet@users.noreply.github.com>
2024-05-30 20:50:05 -0300

    another example question/field for bard

--- src/model/RPGClasses/Bard.java
+++ src/model/RPGClasses/Bard.java
@@ -6,6 +6,7 @@ import view.FileView;
 
 public class Bard extends RPGClass {
     private String instrument;
+    private String style;
 
     public Bard() {
         super("Bard");
@@ -14,6 +15,7 @@ public class Bard extends RPGClass {
     @Override
     public void askQuestions(FileView view) {
         int instrumentChoice = -1;
+        int styleChoice = -1;
 
         do {
             view.displayMessage("What is your instrument?");
@@ -33,6 +35,25 @@ public class Bard extends RPGClass {
                     instrumentChoice = -1; // Reset to continue the loop
             }
         } while (instrumentChoice == -1);
+
+        do {
+            view.displayMessage("What is your style?");
+            view.displayMessage("1. Classical");
+            view.displayMessage("2. Jazz");
+            styleChoice = view.getIntAnswer();
+
+            switch (styleChoice) {
+                case 1:
+                    style = "Classical";
+                    break;
+                case 2:
+                    style = "Jazz";
+                    break;
+                default:
+                    view.displayMessage("Invalid number.");
+                    styleChoice = -1; // Reset to continue the loop
+            }
+        } while (styleChoice == -1);
     }
 
     @Override
@@ -40,6 +61,8 @@ public class Bard extends RPGClass {
         writer.write("Class: " + className);
         writer.newLine();
         writer.write("Instrument: " + instrument);
+        writer.newLine();
+        writer.write("Style: " + style);
         writer.newLine();
     }
 
@@ -50,7 +73,10 @@ public class Bard extends RPGClass {
             if (line.startsWith("Instrument: ")) {
                 instrument = line.substring("Instrument: ".length());
             }
-            if (instrument != null) { // VERY IMPORTANT TO BREAK WHEN FOUND ALL
+            if (line.startsWith("Style: ")) {
+                style = line.substring("Style: ".length());
+            }
+            if (instrument != null && style != null) { // VERY IMPORTANT TO BREAK WHEN FOUND ALL
                 break;
             }
         }
@@ -60,5 +86,6 @@ public class Bard extends RPGClass {
     public void displayData(FileView view) {
         view.displayMessage("Class: " + className);
         view.displayMessage("Instrument: " + instrument);
+        view.displayMessage("Style: " + style);
     }
 }
